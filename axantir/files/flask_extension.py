a5789745dec1ebc917f31069e29f96f4bf96fa76
Adam Sussman <adam.sussman@gmail.com>
2024-11-19 18:47:22 -0800

    do not use app context storage, use extension storage for filestores, pre-instantiate filestores on extension init, not later

--- axantir/files/flask_extension.py
+++ axantir/files/flask_extension.py
@@ -1,41 +1,32 @@
-from typing import Optional
+from typing import Dict, Optional
 
 import werkzeug.utils
-from flask import Flask, current_app
-from flask.globals import app_ctx
+from flask import Flask
 
 from .storage import FileStoreBase
 
 
 class FlaskFileStore(object):
+    app: Flask
+    filestores: Dict[str, FileStoreBase]
+
     def __init__(self, app: Optional[Flask] = None) -> None:
+        self.filestores = {}
         if app is not None:
             self.init_app(app)
 
     def init_app(self, app: Flask) -> None:
+        self.app = app
         app.extensions["axantir_filestore"] = self
 
+        if app.config.get("FILES_STORE_SETTINGS"):
+            for name, settings in self.app.config["FILES_STORE_SETTINGS"].items():
+                storage_class = werkzeug.utils.import_string(settings["class"])
+                self.filestores[name] = storage_class(**settings.get("kwargs", {}))
+
     def get_filestore(self, name: str = "default") -> FileStoreBase:
-        if (
-            app_ctx is not None
-            and (filestores := getattr(app_ctx, "axantir_file_store", None))
-            and (filestore := filestores.get(name))
-        ):
-            return filestore
-
-        if (
-            app_ctx is not None
-            and current_app.config.get("FILES_STORE_SETTINGS")
-            and (settings := current_app.config["FILES_STORE_SETTINGS"].get(name, None))
-        ):
-            storage_class = werkzeug.utils.import_string(settings["class"])
-            filestore = storage_class(**settings.get("kwargs", {}))
-            if not hasattr(app_ctx, "axantir_file_store"):
-                app_ctx.axantir_file_store = {}  # type: ignore
-
-            app_ctx.axantir_file_store[name] = filestore  # type: ignore
-
-            return filestore
+        if name in self.filestores:
+            return self.filestores[name]
 
         raise Exception(
             f"No filestore named `{name}` configured on the current application"
